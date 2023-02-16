838e661c643c094706f3e4b93b548d5f29095a1c
amuMabirimise <awmabirimise@gmail.com>
2023-02-16 14:28:16 -0800

    Size of a float:

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,16 +4,14 @@
  * the computer it is compiled and run on..
  * Return: Always 0.
 */
-
 int main(void)
 
 {
 
 	printf("Size of a char: %zu byte(S)\n", sizeof(char));
-	
 	printf("Size of an int: %zu byte(S)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(S)\n", sizeof(long int));
-	printf("Size of a long |long int: %zu byte(S)\n",sizeof(long long int));
+	printf("Size of a long long int: %zu byte(S)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(S)\n", sizeof(float));
 	return (0);
 }
