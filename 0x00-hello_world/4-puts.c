285c667b894c50d88fa0782259708c08a87f0005
amuMabirimise <awmabirimise@gmail.com>
2023-02-16 12:11:40 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main-Print a line of code using puts
+ * main -Print a line of code using puts
  *
  * Retutn: 0
 */
 
 int main(void)
-{
-         puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+{	
+        puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
