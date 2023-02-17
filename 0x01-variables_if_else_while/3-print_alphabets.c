1701d7b638732feb7c109fc4a3b1eded2f2904aa
amuMabirimise <awmabirimise@gmail.com>
2023-02-17 08:26:10 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include<stdio.h>
+/**
+ * main-  prints the alphabet in lowercase
+ * Return: Always 0 (Succes)
+*/
 int main(void)
 {
-	int n;
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int letter;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	for (letter = 0; letter < 26; letter++)
+	{
+		putchar(alphabet[letter]);
+	}
+	putchar('\n');
 	return (0);
 }
+
