8f4776968f3b41ec482d249717857f5aa463730d
amuMabirimise <awmabirimise@gmail.com>
2023-02-17 10:50:55 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#incluse <stdio.>
+/**
+ * main - Entry point
+ *
+ */
+
+int main(voi)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+
+	c = 'a';
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
