3860d6974cc7dd3ee66950593e41560108aa0296
amuMabirimise <awmabirimise@gmail.com>
2023-02-17 06:45:42 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int ch;
+
+	for ch = 'A';
+	for ch < = 'Z'; ch++;
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 Positive anything is better than negative nothing
 1-last_digit.c
+2-print_alphabet.c
