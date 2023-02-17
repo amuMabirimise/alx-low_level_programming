4869f491134bdb0d2500864e225e68d747a9dbe3
amuMabirimise <awmabirimise@gmail.com>
2023-02-17 09:08:06 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,22 +7,22 @@
 
 int main(void)
 {
-        char c;
+	char c;
 
-        char d;
+	char d;
 
-        c = 'a';
-        d = 'A';
-        while
-                (c <= 'z) {
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z) {
 			putchar(c);
 			c++;
 		}
-        while
-                (d <= 'Z') { 
+	while
+		(d <= 'Z') {
 			putchar(d);
-                        d++;
-                }
-        putchar('\n');
-        return (0);
+			 d++;
+		}
+	putchar('\n');
+	return (0);
 }
