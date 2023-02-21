40837ce53d50c0d2df782173295d6921290b3deb
amuMabirimise <awmabirimise@gmail.com>
2023-02-21 01:54:30 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int char;
+	for (char='A' ;char<='Z' ;char++'\n')
+	putchar (char0;
+	return (0);
+}
