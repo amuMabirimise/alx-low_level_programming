581e9de7f78054676e0444bdd63c9b6d48432672
amuMabirimise <awmabirimise@gmail.com>
2023-02-21 09:54:07 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,26 +2,14 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the number of the sign to be printed
- * Return: 1 if number is greater than zero
- * -1 if number is less than zero
+ * _abs - Computes the absolute value of an integer
+ * @t: The integer to be coomputed
+ * Return: The absolute value
  */
-int print_sign(int n)
+int _abs(int t)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
+	if (t >= 0)
+		return (t);
 	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+		return (-t);
 }
