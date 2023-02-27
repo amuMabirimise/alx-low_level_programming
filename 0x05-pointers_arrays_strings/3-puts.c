599e1472b311d0ad2af7989ef8553185e5385e1e
amuMabirimise <awmabirimise@gmail.com>
2023-02-27 08:16:15 -0800

    3-pus.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,14 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
+ *Return: the length of the string
  */
+
 void _puts(char *str)
 {
 	int a = 0;
 
-	while (*str(a) != '\0')
+	while (str(a) != '\0')
 	{
 		_putchar(str(a));
 		a++;
