bfe6e9dbb0e2de4860d41ce53709e4cfc4209ffd
amuMabirimise <awmabirimise@gmail.com>
2023-03-01 08:21:03 -0800

    0-stcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - A function that concatenates two strings
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char s1[50];
+	char s2[50];
+	char *p1;
+	char *p2;
+
+	_putchar("\n, s1");
+	gets(s1);
+	_putchar("\n, s2");
+	gets(s2);
+
+	p1 = s1;
+	while(*p1 != '\0')
+	{
+		++p1;
+	}
+
+	p2 = s2;
+	while(*p2 != '\0')
+	{
+		*p1 = *p2;
+		p1++;
+		p2++;
+	}
+	*p1 = '\0';
+	_putchar("\nConcatenated String = %s", s1);
+	return 0;
+}
