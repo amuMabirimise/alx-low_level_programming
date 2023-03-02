257b9408c49108ea5402e83ba1d751ed33fc420f
amuMabirimise <awmabirimise@gmail.com>
2023-03-02 07:58:39 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,16 +2,21 @@
 #include <string.h>
 /**
  * _strcmp - compares two strings which are similar
- * @s1:second value
- * @s2: third value
- * Return:pointer to the resulting string
-*/
-int _strcmp(const char *s1, const char *s2)
-{
-	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+ * @s1: input value
+ * @s2: input value
+ * Return: s1[i] - s2[i]
+ */
+
+int _strcmp(char *s1, char *s2)
+i{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-	s1++;
-	s2++;
+	if (s1[i] != s2[i]) {
+	return (s1[i] - s2[i]);
 	}
-	return (*s1 - *s2);
+	i++;
+	}
+	return (s1[i] - s2[i]);
 }
