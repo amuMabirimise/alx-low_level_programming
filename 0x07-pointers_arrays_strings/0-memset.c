761ce9e05eb7416ea36210cc4ed1c12cb9aadcb5
amuMabirimise <awmabirimise@gmail.com>
2023-03-06 02:54:58 -0800

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,29 +2,22 @@
 #include <stdio.h>
 
 /**
- * _memset(): fills the first n bytes of the memory area pointed to by s with the constant byte b 
+ * _memset() - fills the first n bytes of the memory area pointed 
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- *
+ * @n: value
+ * @s: memory
+ * @b: memory
  * Return: s
  */
 
-#include <stdio.h>
-
 char *_memset(char *s, char b, unsigned int n)
 {
-    for (unsigned int i = 0; i < n; i++)
+	char *ptr = s;
+	
+	while (n--)
 	{
-        s[i] = b;
+		*ptr++ = b;
 	}
-	return s;
-	}
-
-int main()
-{
-	char str[20] = "Hello, world!";
-	printf("Before memset: %s\n", str);
-	_memset(str, '$', 7);
-	printf("After memset: %s\n", str);
-	return 0;
+	return (s);
 }
