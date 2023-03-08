a59b7653df538be1e6f30d45dc61f4be194a9f3b
amuMabirimise <awmabirimise@gmail.com>
2023-03-08 01:28:03 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a single spring
+ * @s: input
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	putchar('\n');
+	return;
+	}
+
+	putchar(*s);
+	_puts_recursion(s + 1);
+
+}
