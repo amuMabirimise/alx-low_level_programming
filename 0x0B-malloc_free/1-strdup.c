d540cc3a25d00ef84416fad86e87cc872556ca43
amuMabirimise <awmabirimise@gmail.com>
2023-03-13 06:45:00 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - check the code for ALX School students.
+ * @str: dynamic value
+ *
+ * Return: dup
+ */
+
+char *_strdup(char *str)
+{
+	char *dup = malloc(strlen(str) + 1);
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	while (dup != NULL)
+	{
+	strcpy(dup, str);
+	}
+	return (dup);
+}
