706f1f7f07a41c8ece2f1bfd46b994a9e2dfd7ce
amuMabirimise <awmabirimise@gmail.com>
2023-03-13 06:13:05 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,25 +3,27 @@
 #include "main.h"
 
 /**
- * create_array -  creates an array of chars, and initializes.
+ * create_array -  creates an array of chars, and initializes
  * @size: dynamic value
  * @c: dynamic value
  * Return: array
  */
 
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
-	char *array = malloc(size * sizeof(char c));
+	char *array = malloc(size * sizeof(char));
 	unsigned int i = 0;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
+
 	if (array == NULL)
 	{
 		return (NULL);
 	}
+
 	while (i < size)
 	{
 		array[i] = c;
