c6cf17811d031287e8e18b2101bbd48c711378c4
root <amuMabirimise>
2023-06-07 13:28:17 -0700

    task-5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,45 +1,40 @@
 #include <stdio.h>
 
+int sqrt_helper(int n, int start, int end);
+
 /**
- * _sqrt_recursion - a function that returns the natural square root of a number
+ * _sqrt_recursion - a function that returns the square root of a number
  * @n: input number
- * Return: natural square root of n
+ * Return: square root of n, -1 if not a natural square root
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
-	return -1;
-	}
-	else if (n == 0 || n == 1)
-	{
-	return n;
-	}
-	else
-	{
-	int start = 1;
-	int end = n;
-	int result = -1;
+	return (-1);
+    
+	return (sqrt_helper(n, 0, n));
+}
 
-        while (start <= end)
-	{
-		int mid = (start + end) / 2;
+/**
+ * sqrt_helper - a helper function to calculate the square root of a number
+ * using binary search
+ * @n: input number
+ * @start: start of the range for binary search
+ * @end: end of the range for binary search
+ * Return: square root of n, -1 if not a natural square root
+ */
+int sqrt_helper(int n, int start, int end)
+{
+	int mid = (start + end) / 2;
 
-		if (mid * mid == n)
-		{
-		return mid;
-		}
-		else if (mid * mid < n)
-		{
-		start = mid + 1;
-		result = mid;
-		}
-		else
-		{
-		end = mid - 1;
-		}
-	}
-
-	return result;
-	}
+	if (start > end)
+	return (-1);
+    
+	if (mid * mid == n)
+	return (mid);
+    
+	if (mid * mid > n)
+	return (sqrt_helper(n, start, mid - 1));
+    
+	return (sqrt_helper(n, mid + 1, end));
 }
