537d2960bc49c2ecb2ef016c1b99f0ec81248b24
root <amuMabirimise>
2023-06-13 07:06:33 -0700

    task-2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,26 +10,24 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	size_t length_s1 = strlen(s1);
-	size_t lenght_s2 = strlen(s2);
-	char *concat = (char *)malloc((length_s1 + lenght_s2 + 1) * sizeof(char));
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+	char *concat;
+	const char *empty = "";
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
+	const char *str1 = s1 != NULL ? s1 : empty;
+	const char *str2 = s2 != NULL ? s2 : empty;
+
+	concat = (char *)malloc((len1 = len2 + 1)* sizeof(char));
 
 	if (concat == NULL)
 	{
+		printf("Memory allocation failed.\n");
 		return (NULL);
 	}
 
-	strcpy(concat, s1);
-	strcat(concat, s2);
+	strcpy(concat, str1);
+	strcat(concat, str2);
 
 	return (concat);
 }
