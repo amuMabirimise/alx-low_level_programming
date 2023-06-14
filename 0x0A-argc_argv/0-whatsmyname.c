b0fb96a382429670871ab38ff536b36e4caf8f87
root <amuMabirimise>
2023-06-14 11:21:59 -0700

    task-1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - prints its name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+		return (0);
+}
