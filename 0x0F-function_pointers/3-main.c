4f78f7ed18a5ab7af0be57a6b552b5812e84fafa
root <amuMabirimise>
2023-06-21 12:02:47 -0700

    task-3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,36 +1,44 @@
-#include "3-calc.h"
-#include <stdio.h>
+#include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
 /**
- * main-performs calculation
- * @argc: total arguments
- * @argv: array of arguments
- * Return: integer or error if did not work
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int a, b;
-	int (*calculation)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-			exit(98);
+		exit(98);
 	}
-	if (argv[2][1])
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	calculation = get_op_func(argv[2]);
-	if (calculation == NULL)
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-			exit(99);
+		exit(100);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	printf("%d\n", calculation(a, b));
-return (0);
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+	return (0);
 }
+
