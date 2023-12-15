4371ff33ebbc92fe4230c9f317efbdd304e36093
root <amuMabirimise>
2023-12-14 23:35:01 -0800

    advanced

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include "search_algos.h"
 
@@ -8,40 +9,52 @@
  * @size: Number of elements in the array
  * @value: Value to search for
  *
- * Return: The first index where the value is located, or -1 if not found
+ * Return: Index where value is located, or -1 if not present or array is NULL
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t step = sqrt(size);
-	size_t prev = 0;
-	size_t i = prev - step;
-	
-	if (array == NULL || size == 0)
-	return (-1);
+	size_t step, prev, current;
 
-	printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+	if (!array || size == 0)
+		return (-1);
+
+	step = sqrt(size);
+	prev = 0;
+	current = 0;
+
+	while (current < size && array[current] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", current, array[current]);
+		prev = current;
+		current += step;
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n", prev, min(current, size));
 
 	while (prev < size && array[prev] < value)
 	{
-		if (prev + step < size)
-		{
-			prev += step;
-			printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
-		}
-		else
-		break;
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		prev++;
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n", prev - step, prev);
-
-	while (i <= (prev < size ? prev : size - 1))
+	if (prev < size && array[prev] == value)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-		return (i);
-		i++;
-	
+		printf("Found %d at index: %lu\n", value, prev);
+		return (prev);
 	}
-		return (-1);
+
+	printf("Value not present in array\n");
+	return (-1);
 }
 
+/**
+ * min - Returns the smaller of two values
+ * @a: First value
+ * @b: Second value
+ *
+ * Return: The smaller of the two values
+ */
+size_t min(size_t a, size_t b)
+{
+	return (a < b ? a : b);
+}
